cd9fc0231bc2d3a25e143f9a21d53d0212fc09c8
alumne <alumne@23-17>
2014-04-24 14:24:04 +0200

    branca master

---
+++ prjava04/src/prjava04/Prjava04.java
@@ -0,0 +1,16 @@
+
+package prjava04;
+import java.io.*; import java.net.*;
+
+public class Prjava04 {
+
+    public static void main(String[] args) { 
+        try {
+            InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress();
+            String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+        }
+        catch (IOException e) { e.printStackTrace();
+        }
+    }
+}
